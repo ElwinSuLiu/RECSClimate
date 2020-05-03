57e465aa11e620cfb4121172340f28ece56e6504
elwin su <elwinsuliu@gmail.com>
2020-05-03 16:25:41 +1200

    img

--- RECSClimate/RECSClimate/ImageResourceExtension.cs
+++ RECSClimate/RECSClimate/ImageResourceExtension.cs
@@ -19,7 +19,6 @@ namespace RECSClimate
                 return null;
             }
 
-            // Do your translation lookup here, using whatever method you require
             var imageSource = ImageSource.FromResource(Source, typeof(ImageResourceExtension).GetTypeInfo().Assembly);
 
             return imageSource;
